4ae027712412aea253886cca24697c1bb800962f
shaileshiyer <swethashailu9696@gmail.com>
2016-10-10 21:55:04 +0530

    got index working
    the index is now working and am able to get static effect out of it also
    changed function name of new window

--- clipper.py
+++ clipper.py
@@ -5,55 +5,60 @@ mainlist = []
 buttonlist=[]
 buttonlist1=[]
 
-index=-1
-copydict={}
+##index=-1
+##copydict={}
 root = Tk()
+class staticindex:
+     index=0
 
-def new_window1(message):
-
+indexinstance = staticindex()
+         
+def textwindow1(indexer):
+     print indexer
      win=Toplevel()
      frame1=Frame(win)
      sbar=Scrollbar(frame1)
      text1 = Text(frame1)
-     text1.insert(INSERT,message)
+     text1.insert(INSERT,mainlist[indexer])
      sbar.config(command=text1.yview)
      text1.config(yscrollcommand=sbar.set)
      sbar.pack(side=RIGHT,fill=Y,expand=False)
      text1.pack(side=LEFT,fill=BOTH,expand=True)
      frame1.pack()
 
-def copy1(message):
-    pyperclip.copy(message)
+##def copy1(message):
+##    pyperclip.copy(message)
 
 
 def store():
-    global root,index
+    global root,indexinstance
     spam = pyperclip.paste()
     mainlist.append(spam)
-    index=index+1
-
-    copydict[index]=spam
-    print(copydict)
+    currentindex=indexinstance.index
+    ##index=index+1 
+    ##copydict[index]=spam
+    ##print(copydict)
     frame_create = Frame(root, height=3, width=10)
-    new_window=Button(frame_create, text="Expand",command=lambda : new_window1(spam))
-    copy_button=Button(frame_create,text="Copy",command=lambda : copy1(spam))
+    new_window=Button(frame_create, text="Expand",command=lambda : textwindow1(currentindex))
+    copy_button=Button(frame_create,text="Copy",command=lambda : get(currentindex))
+     
     buttonlist.append(new_window)
     buttonlist1.append(copy_button)
     print(buttonlist)
     text = Text(frame_create, height=5, width=100)
     text.insert(INSERT, spam[0:100])
     text.pack()
-    buttonlist[index].pack()
-    buttonlist1[index].pack()
+    buttonlist[currentindex].pack()
+    buttonlist1[currentindex].pack()
     #print(buttonlist.index(new_window))
     frame_create.pack()
     print(spam)
-
+    indexinstance.index+=1 
 
 def get(index):
+    print index
     item = mainlist[index]
-    print
-    item
+    print item
     pyperclip.copy(item)
 
 
